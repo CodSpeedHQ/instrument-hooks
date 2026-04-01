a51442f412b9901a3582dc74c1f6807ed310a6be
Guillaume Lagrange <guillaume@glagrange.eu>
2026-04-01 12:19:16 +0200

    feat: make sure test failures break the ci
    😰

--- src/tests/runner.zig
+++ src/tests/runner.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 
 pub fn main() !void {
     const out = std.io.getStdOut().writer();
+    var has_failures = false;
     for (builtin.test_functions) |t| {
         std.testing.allocator_instance = .{};
 
@@ -10,14 +11,20 @@ pub fn main() !void {
         const result = t.func();
         if (result) |_| {
             try std.fmt.format(out, "[SUCCESS] {s}\n", .{name});
-        } else |err| {
-            try std.fmt.format(out, "[FAIL] {s}: {}\n", .{ t.name, err });
+        } else |err| switch (err) {
+            error.SkipZigTest => try std.fmt.format(out, "[SKIP] {s}\n", .{name}),
+            else => {
+                has_failures = true;
+                try std.fmt.format(out, "[FAIL] {s}: {}\n", .{ t.name, err });
+            },
         }
 
         if (std.testing.allocator_instance.deinit() == .leak) {
+            has_failures = true;
             try std.fmt.format(out, "{s} leaked memory\n", .{name});
         }
     }
+    if (has_failures) std.process.exit(1);
 }
 
 fn extractName(t: std.builtin.TestFn) []const u8 {
